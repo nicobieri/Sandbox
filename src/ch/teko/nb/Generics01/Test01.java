f8e83fcba0c3711a27214652c60026d58a38edff
nicobieri <nrbieri@outlook.com>
2022-06-20 17:31:40 +0200

    complete

---
+++ src/ch/teko/nb/Generics01/Test01.java
@@ -0,0 +1,18 @@
+package src.ch.teko.nb.Generics01;
+
+class Test01 {
+    /*
+    public static void main(String[] args)
+    {
+        // instance of Integer type
+        typeArguments<Integer> iObj = new Test<Integer>(15);
+        System.out.println(iObj.getObject());
+
+        // instance of String type
+        typeArguments<String> sObj
+                = new typeArguments<>("GeeksForGeeks");
+        System.out.println(sObj.getObject());
+        iObj = sObj; // This results an error
+    }
+    */
+}
